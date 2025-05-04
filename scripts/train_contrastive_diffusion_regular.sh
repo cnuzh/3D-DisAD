906e0694f513529d75cfbef4f79e9159db885745
cnuzh <43660513+cnuzh@users.noreply.github.com>
2025-05-04 16:59:17 +0800

    Delete scripts directory

--- scripts/train_contrastive_diffusion_regular.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Schedule execution of many runs
-# Run from root folder with: bash scripts/train_regular.sh
-
-
-export HYDRA_CONFIG_NAME="train_contrastive_diffusion_regular.yaml"
-python ./src/train.py
